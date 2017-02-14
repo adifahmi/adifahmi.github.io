c68503dc0ac75cc1ff7cf3d9806e5eaea9bd1b20
Adi Fahmi <adifahmi379@gmail.com>
2017-02-14 15:20:20 +0700

    Fade out loading on page load.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -10,3 +10,10 @@
 
 		
 })(jQuery);
+// Pre loader script
+//paste this code under head tag or in a seperate js file.
+// Wait for window load
+$(window).load(function() {
+	// Animate loader off screen
+    $(".se-pre-con").fadeOut("slow");;
+});

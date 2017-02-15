ffef5b9e0eb26761836d57928e2b58d8e73703bb
Adi Fahmi <adifahmi379@gmail.com>
2017-02-15 14:42:12 +0700

    Code cleanup.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,4 @@
 (function($) {
-
 // prettyPhoto
 	jQuery(document).ready(function(){
 		jQuery('a[data-gal]').each(function() {
@@ -7,9 +6,8 @@
 		});  	
 		jQuery("a[data-rel^='prettyPhoto']").prettyPhoto({animationSpeed:'slow',theme:'light_square',slideshow:false,overlay_gallery: false,social_tools:false,deeplinking:false});
 	}); 
-
-		
 })(jQuery);
+
 // Pre loader script
 //paste this code under head tag or in a seperate js file.
 // Wait for window load

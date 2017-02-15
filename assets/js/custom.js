44f891de53c885cf12c77da60954e75845a27f27
Adi Fahmi <adifahmi379@gmail.com>
2017-02-15 14:10:38 +0700

    Move validation to inline scrpt.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -30,40 +30,4 @@ $('a[href*="#"]:not([href="#"])').click(function() {
       return false;
     }
   }
-})(jQuery);
-
-//jQuery form Validation
-// Wait for the DOM to be ready
-$(function() {
-  // Initialize form validation on the registration form.
-  $("form[name='contact_form']").validate({
-    // Specify validation rules
-    rules: {
-      // The key name on the left side is the name attribute
-      // of an input field. Validation rules are defined
-      // on the right side
-      name: "required",
-      email: {
-        required: true,
-        email: true
-      },
-      subject: "required",
-      message: "required"
-    },
-    // Specify validation error messages
-    messages: {
-      name: "This field is required.",
-      subject: "This field is required.",
-      subject: "This field is required.",
-      email: {
-        required: "This field is required.",
-        email: "Please enter a valid email address."
-      }
-    },
-    // Make sure the form is submitted to the destination defined
-    // in the "action" attribute of the form when valid
-    submitHandler: function(form) {
-      form.submit();
-    }
-  });
 });

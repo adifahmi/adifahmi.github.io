ac53fa00112ba265534a6907dd3d5e732f6cc5b1
Adi Fahmi <adifahmi379@gmail.com>
2017-02-15 11:26:57 +0700

    Workaround smoothscrolling.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -19,17 +19,15 @@ $(window).load(function() {
 });
 
 // jQuery for page scrolling feature
-$(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
+$('a[href*="#"]:not([href="#"])').click(function() {
+  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+      return false;
     }
-  });
+  }
 });

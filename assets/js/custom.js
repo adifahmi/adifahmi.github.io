675b7031ae6ee81591c8d1dbd295db886bdaba02
Adi Fahmi <adifahmi379@gmail.com>
2017-02-15 14:22:40 +0700

    Work around smooth scrolling.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -17,19 +17,3 @@ $(window).load(function() {
 	// Animate loader off screen
     $(".se-pre-con").fadeOut("slow");;
 });
-
-// jQuery for page scrolling feature
-(function($) {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-})(jQuery);

226b67177d05c70a4dc85e2f0897a34e749f7831
Adi Fahmi <adifahmi379@gmail.com>
2017-02-15 11:02:30 +0700

    Add smooth page scrolling.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -17,3 +17,14 @@ $(window).load(function() {
 	// Animate loader off screen
     $(".se-pre-con").fadeOut("slow");;
 });
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('body').on('click', '.page-scroll a', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});

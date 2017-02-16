2fa5c0e19207d85b30ebf279e3fe8ca2bef593ea
Adi Fahmi <adifahmi379@gmail.com>
2017-02-16 14:37:21 +0700

    Disable autofocus to invalid input.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -21,6 +21,8 @@ $(window).load(function() {
 (function($) {
   // Initialize form validation on the registration form.
   $("form[name='contact_form']").validate({
+	// Disable autofocus on valid input
+	focusInvalid: false,
     // Specify validation rules
     rules: {
       // The key name on the left side is the name attribute

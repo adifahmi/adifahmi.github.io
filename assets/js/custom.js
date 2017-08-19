a7b10fb88df3dee5d43aa4ec0c74adadc720435c
Adi Fahmi <adifahmi379@gmail.com>
2017-08-19 20:04:12 +0700

    Styling Navbar

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -10,6 +10,15 @@ NProgress.inc(0.2);
     NProgress.done();
 });
 
+//jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+  if ($(".navbar").offset().top > 50) {
+      $(".navbar-fixed-top").addClass("top-nav-collapse");
+  } else {
+      $(".navbar-fixed-top").removeClass("top-nav-collapse");
+  }
+});
+
 (function($) {
 // prettyPhoto
 	jQuery(document).ready(function(){

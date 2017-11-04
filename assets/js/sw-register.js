ea58312c37f0589ef793bef169562d31c124dd16
Adi Fahmi <adifahmi379@gmail.com>
2017-11-04 11:44:08 +0700

    fix path sw.js

--- assets/js/sw-register.js
+++ assets/js/sw-register.js
@@ -2,7 +2,7 @@ if (navigator.serviceWorker.controller) {
   console.log('[PWA Builder] active service worker found, no need to register')
 } else {
   //Register the ServiceWorker
-  navigator.serviceWorker.register('pwabuilder-sw.js', {
+  navigator.serviceWorker.register('/assets/js/sw.js', {
     scope: './'
   }).then(function(reg) {
     console.log('Service worker has been registered for scope:'+ reg.scope);
